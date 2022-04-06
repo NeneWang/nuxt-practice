ede71aa14e46ef240b2a79e150dede64f70b38e0
NelsonWang <wangnelson2@gmail.com>
2022-04-06 15:57:23 -0400

    params | dynamic folder structure

--- my-first-nuxt-app/pages/index.vue
+++ my-first-nuxt-app/pages/index.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <h1>Hello this is my first app!</h1>
+    <section class="container">
+      <nuxt-link to="/users">Users</nuxt-link>
+    </section>
   </div>
 </template>
 
